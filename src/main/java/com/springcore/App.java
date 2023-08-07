a6a426cf6f0628c9d4b4a074c58a13fe37581ae5
sg093895 <sg093895@cerner.net>
2023-08-08 02:29:29 +0530

    Setter injection

--- src/main/java/com/springcore/App.java
+++ src/main/java/com/springcore/App.java
@@ -4,6 +4,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
+  public static final String pass = "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";
   public static void main(String[] args) {
     ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
     Student student = (Student) context.getBean("Student1");
