79a1927aa743eabc94bf3df44505d7e3d0e2bcc7
sg093895 <sg093895@cerner.net>
2023-08-08 22:46:46 +0530

    Setter injection with P schema

--- src/main/java/com/springcore/App.java
+++ src/main/java/com/springcore/App.java
@@ -7,7 +7,9 @@ public class App {
   public static final String pass = "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";
   public static void main(String[] args) {
     ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
-    Student student = (Student) context.getBean("Student1");
-    System.out.println(student);
+    Student student1 = (Student) context.getBean("Student1");
+    Student student2 = (Student) context.getBean("Student2");
+    System.out.println(student1);
+    System.out.println(student2);
   }
 }
